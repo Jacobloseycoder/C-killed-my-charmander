f719642ece3afbc770e4204044275fa0ab1a0949
jacob losey <loseyjacob1@gmail.com>
2026-04-13 13:42:38 -0500

    Add files via upload

---
+++ C++ killed my pokemon.cpp
@@ -0,0 +1,10 @@
+//At least 6 classes that represent different parts of the game (see suggestions below).
+//A main game loop that lets the player explore, make decisions, and progress.
+//Inventory or status tracking(e.g., player has a key, health is low, etc.).
+//At least 10 rooms or scenes with unique interactions.
+//Meaningful user input, such as commands("go north", "take sword", "open door").
+//Win / lose conditions(e.g., escape the castle, find the treasure, survive the monster).
+//Comments and clear, organized code.
+
+//enter game is just a pokemon battle sim 
+//
