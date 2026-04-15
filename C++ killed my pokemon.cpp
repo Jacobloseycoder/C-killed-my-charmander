fa98738b5c500e0db34894c706a2864005bbff34
Jacobloseycoder <loseyjacob1@gmail.com>
2026-04-15 13:42:54 -0500

    Update C++ killed my pokemon.cpp

--- C++ killed my pokemon.cpp
+++ C++ killed my pokemon.cpp
@@ -1,10 +1,44 @@
-//At least 6 classes that represent different parts of the game (see suggestions below).
+//reqirements
+//At least 7 classes that represent different parts of the game(see suggestions below).
 //A main game loop that lets the player explore, make decisions, and progress.
 //Inventory or status tracking(e.g., player has a key, health is low, etc.).
-//At least 10 rooms or scenes with unique interactions.
+//At least 15 rooms or scenes with unique interactions.
 //Meaningful user input, such as commands("go north", "take sword", "open door").
 //Win / lose conditions(e.g., escape the castle, find the treasure, survive the monster).
 //Comments and clear, organized code.
 
-//enter game is just a pokemon battle sim 
 //
+
+//classes
+//pokemon class - holds the pokemons type, stats, and moves
+//player class - holds the inventory
+//battle class - handles the battle mechanics
+//enemy class - holds the enemy pokemons type, stats, and moves
+//riddle class - holds the riddle and answer for the gym leaders
+//pokemon generator class - generates random pokemon for the player to catch and battle
+
+//enter game is just a pokemon battle sim 
+//game formst;
+//1. choose your pokemon starter
+//2. fight agenst rival
+//3. randomly give second pokemon
+//4. first gym
+//5. rendon new pokemon
+//6. second gym
+//7. rendon new pokemon
+//8. third gym
+//9. ether new pokemon or rival battle
+//10. forth gym
+//11. rendon new pokemon
+//12. fifth gym
+//13. rendon new pokemon
+//14. sixth gym
+//15. rendon new pokemon
+//16. seventh gym
+//17. rival battle
+//18. rendon new pokemon
+//19. eighth gym
+//20. rendon new pokemon
+//21. elite four gauntlet
+//22. rival battle for the championship
+//23. end game or maybe a post game with legendaries and stuff idk
