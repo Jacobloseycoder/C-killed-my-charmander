be9c5a3081ddd23ef0193c1f6a2db278a7aebf16
Luke-gif-ui <llbishop41@gmail.com>
2026-04-20 13:03:57 -0500

    hola

--- Meowth/Meowth/MainPokemon.cpp
+++ Meowth/Meowth/MainPokemon.cpp
@@ -1,164 +1,30 @@
 #include "Pokemon.h"
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-	Pokemon pikachu("Pikachu", 100, 50, 10);
-	cout << "Name: " << pikachu.getName() << endl;
-	cout << "HP: " << pikachu.getHp() << endl;
-	cout << "Attack: " << pikachu.getAttack() << endl;
-	cout << "Level: " << pikachu.getLevel() << endl;
-	
-	Pokemon charmander("Charmander", 80, 40, 8);
-	cout << "Name: " << charmander.getName() << endl;
-	cout << "HP: " << charmander.getHp() << endl;
-	cout << "Attack: " << charmander.getAttack() << endl;
-	cout << "Level: " << charmander.getLevel() << endl;
+	fstream inFile("C:\\Users\\2020920\\Desktop\\C-killed-my-charmander\\Meowth\\Meowth\\Pokedex.txt");
+	if (!inFile)
+	{
+		cout << "Error!!!! File not found." << endl;
+		return 0;
+	}
+	else
+	{
+		int hp, attack, level;
+		string name;
+		while (inFile >> name >> hp >> attack >> level)
+		{
+			Pokemon pokemon(name, hp, attack, level);
+			cout << "Name: " << pokemon.getName() << endl;
+			cout << "HP: " << pokemon.getHp() << endl;
+			cout << "Attack: " << pokemon.getAttack() << endl;
+			cout << "Level: " << pokemon.getLevel() << endl;
+			cout << endl;
+		}
 
-	Pokemon Squirtle("Squirtle", 90, 45, 9);
-	cout << "Name: " << Squirtle.getName() << endl;
-	cout << "HP: " << Squirtle.getHp() << endl;
-	cout << "Attack: " << Squirtle.getAttack() << endl;
-	cout << "Level: " << Squirtle.getLevel() << endl;
-
-	Pokemon Bulbasaur("Bulbasaur", 85, 42, 7);
-	cout << "Name: " << Bulbasaur.getName() << endl;
-	cout << "HP: " << Bulbasaur.getHp() << endl;
-	cout << "Attack: " << Bulbasaur.getAttack() << endl;
-	cout << "Level: " << Bulbasaur.getLevel() << endl;
-
-	Pokemon Caterpie("Caterpie", 60, 30, 5);
-	cout << "Name: " << Caterpie.getName() << endl;
-	cout << "HP: " << Caterpie.getHp() << endl;
-	cout << "Attack: " << Caterpie.getAttack() << endl;
-	cout << "Level: " << Caterpie.getLevel() << endl;
-
-	Pokemon Weedle("Weedle", 55, 25, 4);
-	cout << "Name: " << Weedle.getName() << endl;
-	cout << "HP: " << Weedle.getHp() << endl;
-	cout << "Attack: " << Weedle.getAttack() << endl;
-	cout << "Level: " << Weedle.getLevel() << endl;
-
-	Pokemon Pidgey("Pidgey", 70, 35, 6);
-	cout << "Name: " << Pidgey.getName() << endl;
-	cout << "HP: " << Pidgey.getHp() << endl;
-	cout << "Attack: " << Pidgey.getAttack() << endl;
-	cout << "Level: " << Pidgey.getLevel() << endl;
-
-	Pokemon Rattata("Rattata", 65, 28, 5);
-	cout << "Name: " << Rattata.getName() << endl;
-	cout << "HP: " << Rattata.getHp() << endl;
-	cout << "Attack: " << Rattata.getAttack() << endl;
-	cout << "Level: " << Rattata.getLevel() << endl;
-
-	Pokemon Spearow("Spearow", 75, 38, 7);
-	cout << "Name: " << Spearow.getName() << endl;
-	cout << "HP: " << Spearow.getHp() << endl;
-	cout << "Attack: " << Spearow.getAttack() << endl;
-	cout << "Level: " << Spearow.getLevel() << endl;
-
-	Pokemon Ekans("Ekans", 80, 40, 8);
-	cout << "Name: " << Ekans.getName() << endl;
-	cout << "HP: " << Ekans.getHp() << endl;
-	cout << "Attack: " << Ekans.getAttack() << endl;
-	cout << "Level: " << Ekans.getLevel() << endl;
-
-	Pokemon Sandshrew("Sandshrew", 85, 42, 9);
-	cout << "Name: " << Sandshrew.getName() << endl;
-	cout << "HP: " << Sandshrew.getHp() << endl;
-	cout << "Attack: " << Sandshrew.getAttack() << endl;
-	cout << "Level: " << Sandshrew.getLevel() << endl;
-
-	Pokemon Nidoran("Nidoran", 90, 45, 10);
-	cout << "Name: " << Nidoran.getName() << endl;
-	cout << "HP: " << Nidoran.getHp() << endl;
-	cout << "Attack: " << Nidoran.getAttack() << endl;
-	cout << "Level: " << Nidoran.getLevel() << endl;
-
-	Pokemon Clefairy("Clefairy", 95, 48, 11);
-	cout << "Name: " << Clefairy.getName() << endl;
-	cout << "HP: " << Clefairy.getHp() << endl;
-	cout << "Attack: " << Clefairy.getAttack() << endl;
-	cout << "Level: " << Clefairy.getLevel() << endl;
-
-	Pokemon Vulpix("Vulpix", 85, 42, 9);
-	cout << "Name: " << Vulpix.getName() << endl;
-	cout << "HP: " << Vulpix.getHp() << endl;
-	cout << "Attack: " << Vulpix.getAttack() << endl;
-	cout << "Level: " << Vulpix.getLevel() << endl;
-
-	Pokemon Jigglypuff("Jigglypuff", 100, 50, 10);
-	cout << "Name: " << Jigglypuff.getName() << endl;
-	cout << "HP: " << Jigglypuff.getHp() << endl;
-	cout << "Attack: " << Jigglypuff.getAttack() << endl;
-	cout << "Level: " << Jigglypuff.getLevel() << endl;
-
-	Pokemon Zubat("Zubat", 70, 35, 6);
-	cout << "Name: " << Zubat.getName() << endl;
-	cout << "HP: " << Zubat.getHp() << endl;
-	cout << "Attack: " << Zubat.getAttack() << endl;
-	cout << "Level: " << Zubat.getLevel() << endl;
-
-	Pokemon Oddish("Oddish", 80, 40, 8);
-	cout << "Name: " << Oddish.getName() << endl;
-	cout << "HP: " << Oddish.getHp() << endl;
-	cout << "Attack: " << Oddish.getAttack() << endl;
-	cout << "Level: " << Oddish.getLevel() << endl;
-
-	Pokemon Paras("Paras", 75, 38, 7);
-	cout << "Name: " << Paras.getName() << endl;
-	cout << "HP: " << Paras.getHp() << endl;
-	cout << "Attack: " << Paras.getAttack() << endl;
-	cout << "Level: " << Paras.getLevel() << endl;
-
-	Pokemon Venonat("Venonat", 85, 42, 9);
-	cout << "Name: " << Venonat.getName() << endl;
-	cout << "HP: " << Venonat.getHp() << endl;
-	cout << "Attack: " << Venonat.getAttack() << endl;
-	cout << "Level: " << Venonat.getLevel() << endl;
-
-	Pokemon Diglett("Diglett", 60, 30, 5);
-	cout << "Name: " << Diglett.getName() << endl;
-	cout << "HP: " << Diglett.getHp() << endl;
-	cout << "Attack: " << Diglett.getAttack() << endl;
-	cout << "Level: " << Diglett.getLevel() << endl;
-
-	Pokemon Meowth("Meowth", 70, 35, 6);
-	cout << "Name: " << Meowth.getName() << endl;
-	cout << "HP: " << Meowth.getHp() << endl;
-	cout << "Attack: " << Meowth.getAttack() << endl;
-	cout << "Level: " << Meowth.getLevel() << endl;
-
-	Pokemon Psyduck("Psyduck", 80, 40, 8);
-	cout << "Name: " << Psyduck.getName() << endl;
-	cout << "HP: " << Psyduck.getHp() << endl;
-	cout << "Attack: " << Psyduck.getAttack() << endl;
-	cout << "Level: " << Psyduck.getLevel() << endl;
-
-	Pokemon Mankey("Mankey", 75, 38, 7);
-	cout << "Name: " << Mankey.getName() << endl;
-	cout << "HP: " << Mankey.getHp() << endl;
-	cout << "Attack: " << Mankey.getAttack() << endl;
-	cout << "Level: " << Mankey.getLevel() << endl;
-
-	Pokemon Growlithe("Growlithe", 85, 42, 9);
-	cout << "Name: " << Growlithe.getName() << endl;
-	cout << "HP: " << Growlithe.getHp() << endl;
-	cout << "Attack: " << Growlithe.getAttack() << endl;
-	cout << "Level: " << Growlithe.getLevel() << endl;
-
-	Pokemon Mewtwo("Mewtwo", 1, 1000000, 1);
-	cout << "Name: " << Mewtwo.getName() << endl;
-	cout << "HP: " << Mewtwo.getHp() << endl;
-	cout << "Attack: " << Mewtwo.getAttack() << endl;
-	cout << "Level: " << Mewtwo.getLevel() << endl;
-
-	Pokemon Mew("Mew", 10000, 1, 100000000000);
-	cout << "Name: " << Mew.getName() << endl;
-	cout << "HP: " << Mew.getHp() << endl;
-	cout << "Attack: " << Mew.getAttack() << endl;
-	cout << "Level: " << Mew.getLevel() << endl;
-
+	}
 }
